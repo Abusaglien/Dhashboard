723e2498ba4a652a334b3d9b187e06ad00a82eb7
Abusaglien <abuabusagline@gmail.com>
2024-10-17 14:56:12 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,49 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_BASE_URL }),
+  reducerPath: "adminApi",
+  tagTypes: [
+    "User",
+    "Products",
+    "Customers",
+    "Sales",
+    "Admins",
+    "Dashboard",
+  ],
+  endpoints: (build) => ({
+    getUser: build.query({
+      query: (id) => `general/user/${id}`,
+      providesTags: ["User"],
+    }),
+    getProducts: build.query({
+      query: () => "client/products",
+      providesTags: ["Products"],
+    }),
+    getCustomers: build.query({
+      query: () => "client/customers",
+      providesTags: ["Customers"],
+    }),
+    getSales: build.query({
+      query: () => "sales/sales",
+      providesTags: ["Sales"],
+    }),
+    getAdmins: build.query({
+      query: () => "management/admins",
+      providesTags: ["Admins"],
+    }),
+    getDashboard: build.query({
+      query: () => "general/dashboard",
+      providesTags: ["Dashboard"],
+    }),
+  }),
+});
+
+export const {
+  useGetUserQuery,
+  useGetProductsQuery,
+  useGetCustomersQuery,
+  useGetSalesQuery,
+  useGetAdminsQuery,
+  useGetDashboardQuery,
+} = api;
